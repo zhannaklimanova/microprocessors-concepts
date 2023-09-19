1476c7f4f2ba0f695ff576e4cee947fb2135afe2
Nafiz Islam <nafiz.islam1001@gmail.com>
2023-09-18 22:09:26 -0400

    add comments

--- Assembly_C_and_Optimization/Core/Src/custom_functions.c
+++ Assembly_C_and_Optimization/Core/Src/custom_functions.c
@@ -50,9 +50,13 @@ void findTranscendental(float32_t omega, float32_t phi, float32_t *x) {
 		function = arm_cos_f32(omega * (xn) + phi) - (xn) * (xn);
 		function_derivative = -omega * arm_sin_f32(omega * (xn) + phi) - 2 * (xn);
 		xn = xn - (function / function_derivative);
+
+		// keep xn within the bound [-1, 1]
 		if (xn > 1) {
+			// pseudo-pseudo random, ensure a whole range of values are tested
 			xn = (float32_t)i / (float32_t)MAX_ITERATION;
 		} else if (xn < -1) {
+			// pseudo-pseudo random, ensure a whole range of values are tested
 			xn = -(float32_t)i / (float32_t)MAX_ITERATION;
 		}
 
@@ -61,6 +65,7 @@ void findTranscendental(float32_t omega, float32_t phi, float32_t *x) {
 
 //	printf("%f - %f = %f\n", arm_cos_f32(omega * (xn) + phi), xn * xn, function);
 
+	// if it's outside the tolerated range, return nan otherwise return solution
 	if (function > tolerance || function < -tolerance) {
 		*x = 0.0 / 0.0;
 	} else {
