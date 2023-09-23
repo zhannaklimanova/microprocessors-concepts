93e9ab4a4ac6ca586a6a1e80a36d116cd138cba3
Zhanna <zklimanova@yahoo.com>
2023-09-22 22:00:10 -0400

    Fix Blinky_Blinky code

--- Blinky_Blinky/Core/Inc/main.h
+++ Blinky_Blinky/Core/Inc/main.h
@@ -57,10 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED1_Pin GPIO_PIN_5
-#define LED1_GPIO_Port GPIOA
-#define LED2_Pin GPIO_PIN_14
-#define LED2_GPIO_Port GPIOB
+#define LED_Pin GPIO_PIN_14
+#define LED_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
